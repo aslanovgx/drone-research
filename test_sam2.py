620c97da982d3b1fa39d4450f8a56e84fe506be4
DenizEkber <bladebey999@gmail.com>
2026-08-14 13:53:07 +0400

    Add SAM2 segmentation project structure

---
+++ test_sam2.py
@@ -0,0 +1,13 @@
+import torch
+from sam2.build_sam import build_sam2
+
+checkpoint = "checkpoints/sam2.1_hiera_small.pt"
+model_cfg = "configs/sam2.1/sam2.1_hiera_s.yaml"
+
+sam2_model = build_sam2(
+    model_cfg,
+    checkpoint,
+    device="cuda" if torch.cuda.is_available() else "cpu"
+)
+
+print("Model uğurla yükləndi:", type(sam2_model))
